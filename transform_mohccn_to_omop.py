c1ce3dd319c41fbcc594e38d27a693b9fd2bf3ee
Marion <mshadbolt@users.noreply.github.com>
2026-01-20 10:33:29 -0800

    try different fix

--- transform_mohccn_to_omop.py
+++ transform_mohccn_to_omop.py
@@ -742,11 +742,11 @@ class MohccnToOmopTransformer:
 				elif ( omop_table_name == "person"):
 					if ( current_donor_id != omop_record["omop_record"]["person_id"] ):
 						current_donor_id = omop_record["omop_record"]["person_id"]
-						new_record = {omop_record["omop_record"].copy()}
+						new_record = {omop_table_name: omop_record["omop_record"].copy()}
 						del new_record[omop_table_name]["person_id"]
 						results_by_datasets["datasets"][current_dataset_array_index]["dataset"]["linked_records"].append({
-							"person":new_record,
-							"linked_records" : []
+							"person": new_record["person"],
+							"linked_records": []
 						})
 						current_donor_array_index = len(results_by_datasets["datasets"][current_dataset_array_index]["dataset"]["linked_records"])-1
 				elif ( len(skip_errors) == 0 ):
